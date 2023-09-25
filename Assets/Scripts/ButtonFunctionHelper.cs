5ad73b23fd16fb2b028fccec5124b26ee4bffdb0
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2023-09-25 17:42:29 -0400

    Untoggling appearance slots
    it is now possible to untoggle each appearance slot (i.e. no hair, default cloth-y clothes)

--- Assets/Scripts/ButtonFunctionHelper.cs
+++ Assets/Scripts/ButtonFunctionHelper.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonFunctionHelper : MonoBehaviour
 {
+    // Setters
     public void SetHair(int val) { GameManager.Instance.GetCharacter().HairType = val; }
 
     public void SetFace(int val) { GameManager.Instance.GetCharacter().FaceType = val; }
@@ -13,4 +15,11 @@ public class ButtonFunctionHelper : MonoBehaviour
     public void SetPants(int val) { GameManager.Instance.GetCharacter().PantsType = val; }
 
     public void SetShoes(int val) { GameManager.Instance.GetCharacter().ShoesType = val; }
+
+    // Untoggling
+    public void CheckHairUntoggle(ToggleGroup group) { if (!group.AnyTogglesOn()) GameManager.Instance.GetCharacter().HairType = 0; }
+    public void CheckFaceUntoggle(ToggleGroup group) { if (!group.AnyTogglesOn()) GameManager.Instance.GetCharacter().FaceType = 0; }
+    public void CheckShirtUntoggle(ToggleGroup group) { if (!group.AnyTogglesOn()) GameManager.Instance.GetCharacter().ShirtType = 0; }
+    public void CheckPantsUntoggle(ToggleGroup group) { if (!group.AnyTogglesOn()) GameManager.Instance.GetCharacter().PantsType = 0; }
+    public void CheckShoesUntoggle(ToggleGroup group) { if (!group.AnyTogglesOn()) GameManager.Instance.GetCharacter().ShoesType = 0; }
 }
