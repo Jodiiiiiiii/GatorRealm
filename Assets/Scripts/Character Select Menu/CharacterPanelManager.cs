db8db851634dd90a9efb68b11646731984c8543e
Andrew Kohler <awk480@gmail.com>
2023-10-13 11:56:30 -0400

    Animations and visuals for character select posters

--- Assets/Scripts/Character Select Menu/CharacterPanelManager.cs
+++ Assets/Scripts/Character Select Menu/CharacterPanelManager.cs
@@ -84,13 +84,13 @@ public class CharacterPanelManager : MonoBehaviour
     private IEnumerator DoSpawnPanels()
     {
         int onScreenCount = GameManager.Instance.GetCharacterCount(); // A running tally of how many character panels are currently displayed
-
+        yield return new WaitForSeconds(1f);
         Debug.Log(GameManager.Instance.GetCharacterCount());
         for (int i = 0; i < GameManager.Instance.GetCharacterCount(); i++) // While we can still get characters
         {
             GameManager.Instance.SetSelectedCharacterIndex(i);   // Sets the appropriate index for getting the next character
             GameObject charaPanel = Instantiate(panelPrefab, new Vector3(spawnpoints[i].position.x, spawnpoints[i].position.y, spawnpoints[i].position.z), Quaternion.Euler(Vector3.zero), parentCanvas);   // Create the panel at the right position
-            yield return new WaitForSeconds(.1f);                                           // While useful as a visual effect as well, a slight delay is actually necessary for the character display to update
+            yield return new WaitForSeconds(.3f);                                           // While useful as a visual effect as well, a slight delay is actually necessary for the character display to update
             charaPanel.GetComponent<CharacterPanel>().SetValues(i);             // Give that panel the correct index for potential data retrieval
         }
 
