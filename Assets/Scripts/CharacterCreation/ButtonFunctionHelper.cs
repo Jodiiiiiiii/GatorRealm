30cc07c921875ec8cc12cfecf82515c6f47a337d
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2023-10-18 18:54:42 -0400

    fixed button function helper script

--- Assets/Scripts/CharacterCreation/ButtonFunctionHelper.cs
+++ Assets/Scripts/CharacterCreation/ButtonFunctionHelper.cs
@@ -124,53 +124,6 @@ public class ButtonFunctionHelper : MonoBehaviour
     public void CheckShirtUntoggle(ToggleGroup group) { if (!group.AnyTogglesOn()) GameManager.Instance.GetCharacter().ShirtType = 0; }
     public void CheckPantsUntoggle(ToggleGroup group) { if (!group.AnyTogglesOn()) GameManager.Instance.GetCharacter().PantsType = 0; }
     public void CheckShoesUntoggle(ToggleGroup group) { if (!group.AnyTogglesOn()) GameManager.Instance.GetCharacter().ShoesType = 0; }
-    #endregion
-
-    #region STEP 2: PERSONALITY
-
-    private float[] answers = new float[15];
-
-    public void Answer1(float val) { answers[0] = val; }
-    public void Answer2(float val) { answers[1] = val; }
-    public void Answer3(float val) { answers[2] = val; }
-    public void Answer4(float val) { answers[3] = val; }
-    public void Answer5(float val) { answers[4] = val; }
-    public void Answer6(float val) { answers[5] = val; }
-    public void Answer7(float val) { answers[6] = val; }
-    public void Answer8(float val) { answers[7] = val; }
-    public void Answer9(float val) { answers[8] = val; }
-    public void Answer10(float val) { answers[9] = val; }
-    public void Answer11(float val) { answers[10] = val; }
-    public void Answer12(float val) { answers[11] = val; }
-    public void Answer13(float val) { answers[12] = val; }
-    public void Answer14(float val) { answers[13] = val; }
-    public void Answer15(float val) { answers[14] = val; }
-
-    public void Submit(){
-        // Courage to Caution
-        float courageCaution = answers[0] + answers[6] + answers[12];
-        GameManager.Instance.GetCharacter().CourageToCaution = 0.5f + courageCaution;
-
-        // Honesty to Deception
-        float honestyDeception = answers[1] + answers[7] + answers[13];
-        GameManager.Instance.GetCharacter().HonestyToDeception = 0.5f + honestyDeception;
-
-        // Diplomacy to Aggression
-        float diplomacyAggression = answers[2] + answers[8];
-        GameManager.Instance.GetCharacter().DiplomacyToAggression = 0.5f + diplomacyAggression;
-
-        // Empathy to Ruthlessness
-        float empathyRuthlessness = answers[3] + answers[9];
-        GameManager.Instance.GetCharacter().EmpathyToRuthlessness = 0.5f + empathyRuthlessness;
-
-        // Optimism to Pessimism
-        float optimismPessimism = answers[4] + answers[10];
-        GameManager.Instance.GetCharacter().OptimismToPessimism = 0.5f + optimismPessimism;
-
-        // Mysticism to Skepticism
-        float mysticismSkepticism = answers[5] + answers[11] + answers[14];
-        GameManager.Instance.GetCharacter().MysticismToSkepticism = 0.5f + mysticismSkepticism;
-
 
     // Robe button handling
     // resets toggle given group - used to disable pants when robe selected
@@ -228,6 +181,54 @@ public class ButtonFunctionHelper : MonoBehaviour
     }
     #endregion
 
+    #endregion
+
+    #region STEP 2: PERSONALITY
+
+    private float[] answers = new float[15];
+
+    public void Answer1(float val) { answers[0] = val; }
+    public void Answer2(float val) { answers[1] = val; }
+    public void Answer3(float val) { answers[2] = val; }
+    public void Answer4(float val) { answers[3] = val; }
+    public void Answer5(float val) { answers[4] = val; }
+    public void Answer6(float val) { answers[5] = val; }
+    public void Answer7(float val) { answers[6] = val; }
+    public void Answer8(float val) { answers[7] = val; }
+    public void Answer9(float val) { answers[8] = val; }
+    public void Answer10(float val) { answers[9] = val; }
+    public void Answer11(float val) { answers[10] = val; }
+    public void Answer12(float val) { answers[11] = val; }
+    public void Answer13(float val) { answers[12] = val; }
+    public void Answer14(float val) { answers[13] = val; }
+    public void Answer15(float val) { answers[14] = val; }
+
+    public void Submit()
+    {
+        // Courage to Caution
+        float courageCaution = answers[0] + answers[6] + answers[12];
+        GameManager.Instance.GetCharacter().CourageToCaution = 0.5f + courageCaution;
+
+        // Honesty to Deception
+        float honestyDeception = answers[1] + answers[7] + answers[13];
+        GameManager.Instance.GetCharacter().HonestyToDeception = 0.5f + honestyDeception;
+
+        // Diplomacy to Aggression
+        float diplomacyAggression = answers[2] + answers[8];
+        GameManager.Instance.GetCharacter().DiplomacyToAggression = 0.5f + diplomacyAggression;
+
+        // Empathy to Ruthlessness
+        float empathyRuthlessness = answers[3] + answers[9];
+        GameManager.Instance.GetCharacter().EmpathyToRuthlessness = 0.5f + empathyRuthlessness;
+
+        // Optimism to Pessimism
+        float optimismPessimism = answers[4] + answers[10];
+        GameManager.Instance.GetCharacter().OptimismToPessimism = 0.5f + optimismPessimism;
+
+        // Mysticism to Skepticism
+        float mysticismSkepticism = answers[5] + answers[11] + answers[14];
+        GameManager.Instance.GetCharacter().MysticismToSkepticism = 0.5f + mysticismSkepticism;
+    }
     #endregion
 
     #region STEP 3: FINALIZE / CLASS SELECTION
