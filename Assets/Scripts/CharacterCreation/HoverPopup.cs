77260f4ce376c9100e61190255c46c63f9de2bd5
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2023-11-06 14:56:34 -0500

    hover error popups
    error popup that follows mouse when hovering over survey submit or finalize buttons before you are allowed to press them

--- Assets/Scripts/CharacterCreation/HoverPopup.cs
+++ Assets/Scripts/CharacterCreation/HoverPopup.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HoverPopup : MonoBehaviour
 {
@@ -9,6 +10,13 @@ public class HoverPopup : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _headerWhite;
     [SerializeField] private TextMeshProUGUI _headerShadow;
     [SerializeField] private TextMeshProUGUI _body;
+
+    private void Update()
+    {
+        // set position to mouse position
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, transform.position.z);
+    }
 
     public void SetBody(string bodyText)
     {
@@ -26,15 +34,30 @@ public class HoverPopup : MonoBehaviour
         gameObject.SetActive(true);
     }
 
+    public void SurveyErrorPopup(Button button)
+    {
+        if(!button.interactable)
+        {
+            SetHeader("Unable to Submit");
+            SetBody("You must answer all survey questions before you can submit");
+            EnablePopup();
+        }
+    }
+
+    public void FinalizeErrorPopup(Button button)
+    {
+        if (!button.interactable)
+        {
+            SetHeader("Unable to Submit");
+            SetBody("You must select a class before you can finalize");
+            EnablePopup();
+        }
+    }
+
     public void DisablePopup()
     {
         gameObject.SetActive(false);
     }
 
-    private void Update()
-    {
-        // set position to mouse position
-        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, transform.position.z);
-    }
+    
 }
